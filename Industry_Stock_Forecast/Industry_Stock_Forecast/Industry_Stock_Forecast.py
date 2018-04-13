e2d4c5abf499588c1364429b1156f9b6d2511330
Allen Chiang <kuan-lun.chiang17@imperial.ac.uk>
2018-04-13 09:18:40 +0100

    update

--- Industry_Stock_Forecast/Industry_Stock_Forecast/Industry_Stock_Forecast.py
+++ Industry_Stock_Forecast/Industry_Stock_Forecast/Industry_Stock_Forecast.py
@@ -3,7 +3,7 @@ from Time_Series import Report as rpt
 import pandas as pd
 import numpy as np
 
-_data = pd.read_csv(r"C:\Users\user\Desktop\Assignment\Industry-Stock-Forecast\Industry_Stock_Forecast\Industry_Stock_Forecast\Data\Data_2018.csv")
+_data = pd.read_csv(r"C:\Users\USER\Documents\Imperial College London\Spring Module\Big Data and Finane\Assignment\A3\Industry-Stock-Forecast\Industry_Stock_Forecast\Industry_Stock_Forecast\Data\Data_2018.csv")
 _data = _data.drop('Unnamed: 0', axis = 1)
 _data.describe()
 
@@ -11,8 +11,8 @@ _data.describe()
 _colName = _data.columns.tolist()
 _dataDict = {}
 _responseVar = 'target'
-_windowList = [80]
-_paraList = [1,2,3]
+_windowList = np.arange(80,120,1)
+_paraList = np.arange(0.0001,0.001,0.0001)
 _trainDict = {}
 _testDict = {}
 
@@ -35,25 +35,12 @@ assert len(_dataDict) == len(_colName)
 
 ################ Train and Test Split ############################################
 for i in _colName:
-<<<<<<< HEAD
-    _trainDict[i] = _dataDict[i].iloc[0:1000]
-=======
-    _trainDict[i] = _dataDict[i].iloc[0:100]
->>>>>>> d9f526e3d02fc6f241218244f9b2d95998afecdd
+    _trainDict[i] = _dataDict[i].iloc[0:200]
     _testDict[i] = _dataDict[i].drop(_trainDict[i].index)
 
 ######################### SVM #########################################
 from sklearn.svm import SVR
+
 mdl = SVR(kernel = 'rbf', cache_size = 10000)
-<<<<<<< HEAD
-res = tcv.paralell_processing(mdl = mdl, data = _trainDict,responseVar = _responseVar, windowList = _windowList, paramList = _paraList, paraName = 'C', colName = _colName, regress = True, fixed = True, greedy = True, n_jobs = 8, verbose = 50, backend = 'multiprocessing', dr = 'Lasso', drparam = np.arange(0.0001,0.001,0.0001))
-=======
-<<<<<<< HEAD
-res = tcv.paralell_processing(mdl = mdl, data = _trainDict,responseVar = _responseVar, windowList = _windowList, paramList = _paraList, paraName = 'C', colName = _colName, regress = True, fixed = True, greedy = True, n_jobs = 4, verbose = 50, backend = 'multiprocessing', dr = 'None')
-=======
-res = tcv.paralell_processing(mdl = mdl, data = _trainDict,responseVar = _responseVar, windowList = _windowList, paramList = _paraList, paraName = 'C', colName = _colName, regress = True, fixed = True, greedy = True, n_jobs = 8, verbose = 50, backend = 'multiprocessing', dr = 'Lasso', drparam = np.arange(0,1,1))
->>>>>>> d9f526e3d02fc6f241218244f9b2d95998afecdd
->>>>>>> bf47e9f14def1c2a4cd80fe495050616e9c13b75
+res = tcv.paralell_processing(mdl = mdl, data = _trainDict,responseVar = _responseVar, windowList = _windowList, paramList = _paraList, paraName = 'C', colName = _colName, regress = True, fixed = True, greedy = True, n_jobs = 4, verbose = 50, backend = 'multiprocessing', dr = 'Lasso', drparam = np.arange(0.00001,0.0001,0.00001))
 res.report_tuned
-x = res.coefList['Food']
-x
